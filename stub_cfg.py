f82144b69a68abb5ba179ef88a9fe1f8998faed2
Anton Korshunov <korshunov8815@gmail.com>
2024-10-21 19:14:54 +0200

    Added example of bot configuration

---
+++ stub_cfg.py
@@ -0,0 +1,12 @@
+# Bot token issued by Telegram
+bot_token = ""
+# Payments token issued by Telegram for your bot
+payments_token = ""
+# Location of VPN server. Should match at least one entry from servers table (by location field)
+vpn_location = "TEST"
+# Telegram ID of owner of the bot. Owner will be notified about users' actions
+owner_id = 0
+# Amount (in bytes) of trafic for free key
+free_bytes_limit = 3000000000
+# Amount (in bytes) of trafic for paid key
+paid_bytes_limit = 30000000000
